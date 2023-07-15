5646a0a57ee3b7621b5da3e7c00998e262e9d5a4
Jared <brymerjared@gmail.com>
2023-07-15 15:57:01 -0500

    fixed html to look like example and updated js

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,4 @@
 var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=6788bcc0725766846d2e8c90df441b50"
 // "https://api.openweathermap.org/data/2.5/weather?q={CITY-NAME}&appid={API-KEY}" 
-
-function getApi(requestUrl) {
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then( function(data){
-            console.log(data);
-        });
-}
-
-getApi(requestUrl);
-
-$('#searchBtn').on('click', function () {
-    var cityName = $('#cityInput').val();
-    console.log(cityName);
-
-    let reqUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=6788bcc0725766846d2e8c90df441b50`;
-    getApi(reqUrl);
-})
+// "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+var inputCity
